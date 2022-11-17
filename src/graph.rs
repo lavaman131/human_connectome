4154c7d17e7a5a0919dcab7dbb4cdc500b54aed0
lavaman131 <lavaalex3@gmail.com>
2022-11-17 13:57:56 -0500

    created counting n-cycles algorithm

--- src/graph.rs
+++ src/graph.rs
@@ -1,80 +1,69 @@
 #![allow(non_snake_case)]
-use std::collections::HashMap;
 // assumption that graph is undirected
 pub struct WeightedGraph {
-    pub weighted_adjacency_matrix: Vec<Vec<f64>>
+    pub weighted_adjacency_matrix: Vec<Vec<f64>>,
 }
 
 impl WeightedGraph {
     pub fn new(weighted_adjacency_matrix: Vec<Vec<f64>>) -> Self {
         WeightedGraph {
-            weighted_adjacency_matrix
+            weighted_adjacency_matrix,
         }
     }
 
-    pub fn count_n_sided_polygons(&self) -> HashMap<i32, i32> {
-        let mut num_sides: HashMap<i32, i32> = HashMap::from([
-            (3, 0),
-            (4, 0),
-            (5, 0)
-        ]);
-        /* Algorithm:
-            1. raise matrix to the power of n
-            2. divide by 2*n since graph is undirected and every vertex is counted
-        */
-        
-        for (k, _) in &num_sides.clone() {
-            let mut A: Vec<Vec<f64>> = self.weighted_adjacency_matrix.clone();
-            // convert to binary representation
-            for i in 0..A.len() {
-                for j in 0..A[i].len() {
-                    A[i][j] = if (A[i][j] as f32) != 0.0 { 1.0 } else { 0.0 };
-                }
-            }
-            A = Self::power_matrix(A, *k);
-            let trace: f64 = Self::find_trace(&A);
-            let mut factorial: i32 = 1;
-            for i in 1..=*k {
-                factorial *= i;
-            }
-            let num_polygons = trace / (factorial as f64);
-            *num_sides.get_mut(k).unwrap() = num_polygons as i32;
-            
-        }
-        num_sides
-    }
-
-    fn power_matrix(mut A: Vec<Vec<f64>>, power: i32) -> Vec<Vec<f64>> {
-        // finds matrix A to the power of n
-        let B: Vec<Vec<f64>> = A.clone();
-        for _ in 1..power {
-            A = Self::matmul(&A, &B);
-        }
-        A
-    }
-
-    fn matmul(A: &Vec<Vec<f64>>, B: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-        // first check if matmul can happen
-        assert_eq!(A[0].len(), B.len());
-
-        // create zero matrix for matmul
-        let mut C: Vec<Vec<f64>> = vec![vec![0.0_f64; A[0].len()]; A.len()];
-
-        for i in 0..C.len() {
-            for j in 0..C[0].len() {
-                for k in 0..C[0].len() {
-                    C[i][j] += A[i][k] * B[k][j];
+    pub fn get_adjacency_matrix(&self) -> Vec<Vec<i32>> {
+        let num_rows: usize = self.weighted_adjacency_matrix.len();
+        let num_cols: usize = self.weighted_adjacency_matrix[0].len();
+        let mut adjacency_matrix: Vec<Vec<i32>> = vec![vec![0; num_cols]; num_rows];
+        for i in 0..num_rows {
+            for j in 0..num_cols {
+                if self.weighted_adjacency_matrix[i][j] > 0.0 {
+                    adjacency_matrix[i][j] = 1;
                 }
             }
         }
-        C
+        adjacency_matrix
     }
 
-    fn find_trace(A: &Vec<Vec<f64>>) -> f64 {
-        let mut trace: f64 = 0.0;
-        for i in 0..A.len() {
-            trace += A[i][i];
+    pub fn count_cycles(&self, n: usize) -> i32 {
+        let graph: Vec<Vec<i32>> = self.get_adjacency_matrix();
+        let V: usize = graph.len();
+        let mut marked: Vec<bool> = vec![false; V];
+        let mut count: i32 = 0;
+        for i in 0..(V - (n - 1)) {
+            count = Self::dfs(&graph, &mut marked, n - 1, i, i, count, V);
+            marked[i] = true;
         }
-        trace
+        count / 2
+    }
+
+    fn dfs(
+        adjacency_matrix: &Vec<Vec<i32>>,
+        marked: &mut Vec<bool>,
+        n: usize,
+        vertex: usize,
+        start: usize,
+        mut count: i32,
+        V: usize,
+    ) -> i32 {
+        marked[vertex] = true;
+
+        if n == 0 {
+            marked[vertex] = false;
+
+            if adjacency_matrix[vertex][start] == 1 {
+                count += 1;
+                return count;
+            } else {
+                return count;
+            }
+        }
+        for i in 0..V {
+            if !marked[i] && adjacency_matrix[vertex][i] == 1 {
+                count = Self::dfs(adjacency_matrix, marked, n - 1, i, start, count, V);
+            }
+        }
+        marked[vertex] = false;
+        count
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ pub mod graph;
 use graph::WeightedGraph;
 
 fn main() {
-    let graph: WeightedGraph = WeightedGraph::new(
-        vec![vec![0.0, 1.0, 1.0], vec![1.0, 0.0, 1.0], vec![1.0, 1.0, 0.0]]
-    );
-    println!("{:?}", &graph.count_n_sided_polygons());
+    let graph: WeightedGraph = WeightedGraph::new(vec![
+        vec![0.0, 1.0, 0.0, 1.0, 0.0],
+        vec![1.0, 0.0, 1.0, 0.0, 1.0],
+        vec![0.0, 1.0, 0.0, 1.0, 0.0],
+        vec![1.0, 0.0, 1.0, 0.0, 1.0],
+        vec![0.0, 1.0, 0.0, 1.0, 0.0],
+    ]);
+    println!("{:?}", &graph.count_cycles(4));
 }
